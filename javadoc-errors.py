9e5a16b78d5422566558e846b7188a65c9cd66c2
Conor Cleary <conor.cleary@oracle.com>
2020-09-24 13:55:58 +0100

    Improved usage and now allows for multiple module checks

--- javadoc-errors.py
+++ javadoc-errors.py
@@ -1,7 +1,15 @@
 import argparse
 import subprocess
 
+
+# Help messages
+msg_jdk_src_dir = "Specify source directory of openjdk i.e. ~/Workspace/openjdk/src"
+msg_api_build_dir = "Specify build output i.e ~/Workspace/openjdk/build/<platform-specific-build>"
+msg_modules = "Comma seperated list of modules to inspect i.e java.base,java.desktop,java.compiler"
+msg_xdoclint_option = "Xdoclint option to use. Enable or disable specific checks for problems in javadoc. Options are accessibility, html, missing, reference, or syntax."
+
 # Assign default values
+env_jdk_src_dir = ""
 env_api_build_dir = ""
 env_modules = []
 env_xdoclint_option = ""
@@ -9,27 +17,40 @@ env_xdoclint_option = ""
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--api-build-dir', help='api-build-dir help')
-    parser.add_argument('--modules', help='modules help')
-    parser.add_argument('--xdoclint-option', help='xdoclint-option help')
+    parser.add_argument('--jdk-src-dir', help=msg_jdk_src_dir)
+    parser.add_argument('--api-build-dir', help=msg_api_build_dir)
+    parser.add_argument('--modules', help=msg_modules)
+    parser.add_argument('--xdoclint-option', help=msg_xdoclint_option)
     args = parser.parse_args()
+    global env_jdk_src_dir
     global env_api_build_dir
     global env_modules
     global env_xdoclint_option
+    env_jdk_src_dir = args.jdk_src_dir
     env_api_build_dir = args.api_build_dir
     env_xdoclint_option = args.xdoclint_option
-    env_modules = args.modules
+    modules = args.modules
+    env_modules = modules.split(",")
 
 
 def run_javadoc():
+    print(env_jdk_src_dir)
     print(env_api_build_dir)
     print(env_modules)
     print(env_xdoclint_option)
-    val = subprocess.check_call(["sh",
-                                 "./javadoc-scripts/doc_command_generic.sh",
-                                 env_api_build_dir, env_modules,
-                                 env_xdoclint_option, "2>",
-                                 str("logs/" + env_xdoclint_option + ".log")])
+    for module in env_modules:
+        print(module)
+        try:
+            err_log = open(str("./logs/" + module + "_" + env_xdoclint_option
+                               + ".log"), "w")
+            cmd = ["sh", "./javadoc-scripts/doc_command_generic.sh",
+                   env_jdk_src_dir, env_api_build_dir, module,
+                   env_xdoclint_option]
+            subprocess.check_call(cmd, stderr=err_log)
+        except subprocess.CalledProcessError as sub_err:
+            print(sub_err)
+        finally:
+            print("Done")
 
 
 def main():
