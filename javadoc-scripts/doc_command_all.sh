9917386e2a216a6ec09096cbf393dc882273b971
Conor Cleary <conor.cleary@oracle.com>
2020-09-24 12:37:09 +0100

    Reorganised scripts & created a generic script for use with a seperate python script

---
+++ javadoc-scripts/doc_command_all.sh
@@ -0,0 +1,28 @@
+/usr/lib/jvm/jdk-14.0.1/bin/java -Xms64M -Xmx1600M -Duser.language=en -Duser.country=US -XX:+UnlockDiagnosticVMOptions -XX:-VerifySharedSpaces \
+-XX:SharedArchiveFile=$API_BUILD_DIR/configure-support/classes.jsa -Xshare:auto \
+-Djava.awt.headless=true -Dextlink.spec.version=16 -Djspec.version=16 -DenableModuleGraph=true \
+--limit-modules java.base,jdk.zipfs,java.compiler.interim,jdk.compiler.interim,jdk.javadoc.interim \
+--add-modules java.compiler.interim,jdk.compiler.interim,jdk.javadoc.interim \
+--module-path $API_BUILD_DIR/buildtools/interim_langtools_modules \
+--add-exports java.base/sun.reflect.annotation=jdk.compiler.interim \
+--add-exports java.base/jdk.internal.jmod=jdk.compiler.interim --add-exports java.base/jdk.internal.misc=jdk.compiler.interim \
+--add-exports java.base/sun.invoke.util=jdk.compiler.interim -m jdk.javadoc.interim/jdk.javadoc.internal.tool.Main \
+-d $API_BUILD_DIR/images/docs/api \
+-use -keywords -notimestamp -serialwarn -encoding ISO-8859-1 -docencoding UTF-8 -breakiterator -splitIndex \
+--system none -javafx --expand-requires transitive --override-methods=summary -tag beaninfo:X -tag revised:X \
+-tag since.unbundled:X -tag spec:X -tag specdefault:X -tag Note:X -tag ToDo:X -tag 'apiNote:a:API Note:' \
+-tag 'implSpec:a:Implementation Requirements:' -tag 'implNote:a:Implementation Note:' -tag param -tag return \
+-tag throws -taglet build.tools.taglet.JSpec\$JLS -taglet build.tools.taglet.JSpec\$JVMS -taglet build.tools.taglet.ModuleGraph \
+-taglet build.tools.taglet.ToolGuide -tag since -tag serialData -tag factory -tag see -taglet build.tools.taglet.ExtLink \
+-taglet build.tools.taglet.Incubating -taglet build.tools.taglet.Preview \
+-tagletpath $API_BUILD_DIR/buildtools/jdk_tools_classes \
+--module-source-path "$API_BUILD_DIR/support/gensrc/*:$API_BUILD_DIR/src/*/linux/classes:$API_BUILD_DIR/src/*/unix/classes:$API_BUILD_DIR/src/*/share/classes" \
+--module java.base \
+-Xdoclint:all, -Xdoclint/package:-org.w3c.* \
+-doctitle 'Java<sup>&reg;</sup> Platform, Standard Edition &amp;&nbsp;Java&nbsp;Development&nbsp;Kit<br>Version 16 API Specification' \
+-windowtitle 'Java SE 16 & JDK 16 [ad-hoc build]' -header '<div style="margin-top: 9px;"><strong>Java SE 16 &amp; JDK 16</strong> <br><strong>DRAFT 16-internal+0-adhoc.conor.open</strong></div>' \
+-bottom '<a href="https://bugreport.java.com/bugreport/">Report a bug or suggest an enhancement</a><br> For further API reference and developer documentation see the <a href="https://docs.oracle.com/pls/topic/lookup?ctx=javase16&amp;id=homepage" target="_blank">Java SE Documentation</a>, which contains more detailed, developer-targeted descriptions with conceptual overviews, definitions of terms, workarounds, and working code examples.<br> Java is a trademark or registered trademark of Oracle and/or its affiliates in the US and other countries.<br> <a href="{@docroot}/../legal/copyright.html">Copyright</a> &copy; 1993, 2020, Oracle and/or its affiliates, 500 Oracle Parkway, Redwood Shores, CA 94065 USA.<br>All rights reserved. Use is subject to <a href="https://www.oracle.com/java/javase/terms/license/java16speclicense.html">license terms</a> and the <a href="https://www.oracle.com/technetwork/java/redist-137594.html">documentation redistribution policy</a>. <br><strong>DRAFT 16-internal+0-adhoc.conor.open</strong> <!-- Version 16-internal+0-adhoc.conor.open -->' \
+-top '<div style="padding: 6px; text-align: center; font-size: 80%; font-family: DejaVu Sans, Arial, Helvetica, sans-serif; font-weight: normal;">This specification is not final and is subject to change. Use is subject to <a href="https://www.oracle.com/java/javase/terms/license/java16speclicense.html">license terms</a>.</div>' \
+-overview $API_BUILD_DIR/support/docs/JDK_API-overview.html \
+-group "Java SE" "java.base:java.compiler:java.datatransfer:java.desktop:java.instrument:java.logging:java.management:java.management.rmi:java.naming:java.net.http:java.prefs:java.rmi:java.scripting:java.se:java.security.jgss:java.security.sasl:java.sql:java.sql.rowset:java.transaction.xa:java.xml:java.xml.crypto" \
+-group "JDK" "jdk.*" -Xmaxerrs 99999 -Xmaxwarns 99999 -quiet
