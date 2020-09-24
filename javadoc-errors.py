b154f0855a6c0ff46db10720b12b9efb5c6d48b3
Conor Cleary <conor.cleary@oracle.com>
2020-09-24 17:18:21 +0100

    Multiple lint options in one run

--- javadoc-errors.py
+++ javadoc-errors.py
@@ -20,37 +20,40 @@ def arg_parser():
     parser.add_argument('--jdk-src-dir', help=msg_jdk_src_dir)
     parser.add_argument('--api-build-dir', help=msg_api_build_dir)
     parser.add_argument('--modules', help=msg_modules)
-    parser.add_argument('--xdoclint-option', help=msg_xdoclint_option)
+    parser.add_argument('--xdoclint-options', help=msg_xdoclint_option)
     args = parser.parse_args()
     global env_jdk_src_dir
     global env_api_build_dir
     global env_modules
-    global env_xdoclint_option
+    global env_xdoclint_options
     env_jdk_src_dir = args.jdk_src_dir
     env_api_build_dir = args.api_build_dir
-    env_xdoclint_option = args.xdoclint_option
+    env_xdoclint_options = args.xdoclint_options
     modules = args.modules
+    options = args.xdoclint_options
     env_modules = modules.split(",")
+    env_xdoclint_options = options.split(",")
 
 
 def run_javadoc():
     print(env_jdk_src_dir)
     print(env_api_build_dir)
     print(env_modules)
-    print(env_xdoclint_option)
+    print(env_xdoclint_options)
     for module in env_modules:
         print(module)
-        try:
-            err_log = open(str("./logs/" + module + "_" + env_xdoclint_option
+        for option in env_xdoclint_options:
+            err_log = open(str("./logs/" + module + "_" + option
                                + ".log"), "w")
             cmd = ["sh", "./javadoc-scripts/doc_command_generic.sh",
                    env_jdk_src_dir, env_api_build_dir, module,
-                   env_xdoclint_option]
-            subprocess.check_call(cmd, stderr=err_log)
-        except subprocess.CalledProcessError as sub_err:
-            print(sub_err)
-        finally:
-            print("Done")
+                   option]
+            try:
+                subprocess.check_call(cmd, stderr=err_log)
+            except subprocess.CalledProcessError as sub_err:
+                print(sub_err)
+            finally:
+                print("Done")
 
 
 def main():
