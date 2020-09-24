7e1912b0a6d5d4bc64421b6665e63e2e8a7b54f0
Conor Cleary <conor.cleary@oracle.com>
2020-09-24 13:55:14 +0100

    Included jdk src directory option

--- javadoc-scripts/doc_command_generic.sh
+++ javadoc-scripts/doc_command_generic.sh
@@ -1,9 +1,9 @@
 /usr/lib/jvm/jdk-14.0.1/bin/java -Xms64M -Xmx1600M -Duser.language=en -Duser.country=US -XX:+UnlockDiagnosticVMOptions -XX:-VerifySharedSpaces \
--XX:SharedArchiveFile=$1/configure-support/classes.jsa -Xshare:auto \
+-XX:SharedArchiveFile=$2/configure-support/classes.jsa -Xshare:auto \
 -Djava.awt.headless=true -Dextlink.spec.version=16 -Djspec.version=16 -DenableModuleGraph=true \
 --limit-modules java.base,jdk.zipfs,java.compiler.interim,jdk.compiler.interim,jdk.javadoc.interim \
 --add-modules java.compiler.interim,jdk.compiler.interim,jdk.javadoc.interim \
---module-path $1/buildtools/interim_langtools_modules \
+--module-path $2/buildtools/interim_langtools_modules \
 --add-exports java.base/sun.reflect.annotation=jdk.compiler.interim \
 --add-exports java.base/jdk.internal.jmod=jdk.compiler.interim --add-exports java.base/jdk.internal.misc=jdk.compiler.interim \
 --add-exports java.base/sun.invoke.util=jdk.compiler.interim -m jdk.javadoc.interim/jdk.javadoc.internal.tool.Main \
@@ -15,14 +15,14 @@
 -tag throws -taglet build.tools.taglet.JSpec\$JLS -taglet build.tools.taglet.JSpec\$JVMS -taglet build.tools.taglet.ModuleGraph \
 -taglet build.tools.taglet.ToolGuide -tag since -tag serialData -tag factory -tag see -taglet build.tools.taglet.ExtLink \
 -taglet build.tools.taglet.Incubating -taglet build.tools.taglet.Preview \
--tagletpath $1/buildtools/jdk_tools_classes \
---module-source-path "$1/support/gensrc/*:/home/conor/Desktop/Oracle/openjdk/src/*/linux/classes:/home/conor/Desktop/Oracle/openjdk/src/*/linux/classes:/home/conor/Desktop/Oracle/openjdk/src/*/unix/classes:/home/conor/Desktop/Oracle/openjdk/src/*/unix/classes:/home/conor/Desktop/Oracle/openjdk/src/*/share/classes:/home/conor/Desktop/Oracle/openjdk/src/*/share/classes" \
---module $2 \
--Xdoclint:$3, -Xdoclint/package:-org.w3c.* \
--doctitle 'Java<sup>&reg;</sup> Platform, Standard Edition &amp;&nbsp;Java&nbsp;Development&nbsp;Kit<br>Version 16 API Specification' \
+-tagletpath $2/buildtools/jdk_tools_classes \
+--module-source-path "$2/support/gensrc/*:$1/*/linux/classes:$1/*/linux/classes:$1/*/unix/classes:$1/*/unix/classes:$1/*/share/classes:$1/*/share/classes" \
+--module $3 \
+-Xdoclint:$4, -Xdoclint/package:-org.w3c.* \
+-doctitle 'Java<sup>&reg;</sup> Platform, Standard Edition &amp;&nbsp;Java&nbsp;Development&nbsp;Kit<br>Version 26 API Specification' \
 -windowtitle 'Java SE 16 & JDK 16 [ad-hoc build]' -header '<div style="margin-top: 9px;"><strong>Java SE 16 &amp; JDK 16</strong> <br><strong>DRAFT 16-internal+0-adhoc.conor.open</strong></div>' \
 -bottom '<a href="https://bugreport.java.com/bugreport/">Report a bug or suggest an enhancement</a><br> For further API reference and developer documentation see the <a href="https://docs.oracle.com/pls/topic/lookup?ctx=javase16&amp;id=homepage" target="_blank">Java SE Documentation</a>, which contains more detailed, developer-targeted descriptions with conceptual overviews, definitions of terms, workarounds, and working code examples.<br> Java is a trademark or registered trademark of Oracle and/or its affiliates in the US and other countries.<br> <a href="{@docroot}/../legal/copyright.html">Copyright</a> &copy; 1993, 2020, Oracle and/or its affiliates, 500 Oracle Parkway, Redwood Shores, CA 94065 USA.<br>All rights reserved. Use is subject to <a href="https://www.oracle.com/java/javase/terms/license/java16speclicense.html">license terms</a> and the <a href="https://www.oracle.com/technetwork/java/redist-137594.html">documentation redistribution policy</a>. <br><strong>DRAFT 16-internal+0-adhoc.conor.open</strong> <!-- Version 16-internal+0-adhoc.conor.open -->' \
 -top '<div style="padding: 6px; text-align: center; font-size: 80%; font-family: DejaVu Sans, Arial, Helvetica, sans-serif; font-weight: normal;">This specification is not final and is subject to change. Use is subject to <a href="https://www.oracle.com/java/javase/terms/license/java16speclicense.html">license terms</a>.</div>' \
--overview $1/support/docs/JDK_API-overview.html \
+-overview $2/support/docs/JDK_API-overview.html \
 -group "Java SE" "java.base:java.compiler:java.datatransfer:java.desktop:java.instrument:java.logging:java.management:java.management.rmi:java.naming:java.net.http:java.prefs:java.rmi:java.scripting:java.se:java.security.jgss:java.security.sasl:java.sql:java.sql.rowset:java.transaction.xa:java.xml:java.xml.crypto" \
 -group "JDK" "jdk.*" -Xmaxerrs 99999 -Xmaxwarns 99999 -quiet
